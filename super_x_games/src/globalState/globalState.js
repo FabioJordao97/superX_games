8f46ba2f5ecdd485fa0e31d7f23f4f4dcdb263f3
Fábio <fabiojordao97@yahoo.com.br>
2021-10-01 18:28:25 -0300

    Configuração básica do app e instalação das libs necessárias.

---
+++ super_x_games/src/globalState/globalState.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import GlobalStateContext from './globalStateContext'
+import productsData from '../productsData/products.json'
+
+const GlobalState = (props) => {
+const products = useState(productsData)
+const [cart, setCart] = useState([])
+const [counter, setCounter] = useState(0)
+const [subTotal, setSubTotal] = useState(0)
+const [total, setTotal] = useState(0)
+
+const data = {products, cart, setCart, counter, setCounter, subTotal, setSubTotal, total, setTotal}
+
+return <GlobalStateContext.Provider value={data}>{props.children}</GlobalStateContext.Provider>
+}
+
+export default GlobalState

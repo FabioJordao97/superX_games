919bd6b7359b892861a0d8415c97d2ec4de9ed77
Fábio <fabiojordao97@yahoo.com.br>
2021-10-01 22:11:17 -0300

    Configuração básica do carrinho.

--- super_x_games/src/components/cartCard.js
+++ super_x_games/src/components/cartCard.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import GlobalStateContext from '../globalState/globalStateContext'
 
 const CartCard = (props) => {
+
+    const {cart, setCart} = useContext(GlobalStateContext)
+
+    const increaseQuantity = (productId) => {
+        const newCart = cart.map((product) => {
+            if(productId === product.id){
+                return{
+                    ...product, quantity: product.quantity + 1
+                }
+            }
+                return product
+          })
+            setCart(newCart)
+
+    }
+
+    const decreaseQuantity = (productID) => {
+        const newProductInCart = cart.map((product) => {
+            if(productID === product.id) {
+                return {
+                    ...product,
+                    quantity: product.quantity -1
+                }
+            }
+                return product
+        })
+            .filter((product) => product.quantity > 0);
+
+            setCart(newProductInCart)
+    }
     return (
         <div>
              <p>{props.id}</p>
@@ -10,6 +41,8 @@ const CartCard = (props) => {
             <p>{props.quantity}</p>
             <p>{props.shipping}</p>
             <img src={props.image} alt={props.name}/>
+            <button onClick={() => increaseQuantity(props.id)}>Adicionar mais um</button>
+            <button onClick={() => decreaseQuantity(props.id)}>Remover um</button>
         </div>
     )
 }
--- super_x_games/src/screens/cartPage.js
+++ super_x_games/src/screens/cartPage.js
@@ -1,9 +1,37 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import CartCard from '../components/cartCard'
 import GlobalStateContext from '../globalState/globalStateContext'
 
 const CartPage = () => {
-    const {cart} = useContext(GlobalStateContext)
+    const {cart, subTotal, setSubTotal, totalItems, setTotalItems, shipping, setShipping, total, setTotal} = useContext(GlobalStateContext)
+
+    const cartTotal = () => {
+        let product = 0;
+        let price = 0;
+        let shipping = 0
+    
+        cart.forEach((products) => {
+          product += products.quantity;
+          price += products.quantity * products.price
+          shipping += products.quantity * 10.00
+        })
+    
+        setTotalItems(product)
+        setSubTotal(price.toFixed(2))
+        setShipping(shipping.toFixed(2))
+
+        if(price > 250){
+            setShipping(0.00)
+            setTotal(price.toFixed(2))
+        } else {
+            setTotal(price + shipping)
+        }
+    
+      }
+
+      useEffect(() => {
+        cartTotal()
+      }, [cart, totalItems, subTotal, shipping, setSubTotal, setTotalItems, setShipping])
     return (
         <div>
              {cart.map((product)=>{
@@ -19,6 +47,10 @@ const CartPage = () => {
                     />
                 )
             })}
+            <p>Quantidade: {totalItems}</p>
+            <p>Frete: {shipping}</p>
+            <p>Subtotal: {subTotal}</p>
+            <p>Total: {total}</p>
         </div>
     )
 }
